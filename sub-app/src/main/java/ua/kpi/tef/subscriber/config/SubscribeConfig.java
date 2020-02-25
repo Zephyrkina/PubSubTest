60c37634afbbcbd2ba0ca313b02c0b0c96e7ad9c
Zephyrkina <alma001@ukr.net>
2020-02-25 17:48:11 +0200

    getting back thread sleep

--- sub-app/src/main/java/ua/kpi/tef/subscriber/config/SubscribeConfig.java
+++ sub-app/src/main/java/ua/kpi/tef/subscriber/config/SubscribeConfig.java
@@ -39,6 +39,11 @@ public class SubscribeConfig {
     @ServiceActivator(inputChannel = "pubsubInputChannel")
     public MessageHandler messageReceiver() {
         return message -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             log.info("Message arrived! Payload: {} ", new String((byte[]) message.getPayload()));
             BasicAcknowledgeablePubsubMessage originalMessage =
                     message.getHeaders().get(GcpPubSubHeaders.ORIGINAL_MESSAGE, BasicAcknowledgeablePubsubMessage.class);
